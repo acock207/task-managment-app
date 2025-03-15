fd11233e66b23299e58f2721ad8e8cb22ddee1d4
Kolade Shittu <acock207@gmail.com>
2025-03-15 14:41:09 +0100

    first commit

--- task-management-app/vite.config.js
+++ task-management-app/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+          'mui': ['@mui/material', '@mui/icons-material'],
+          'dnd': ['react-beautiful-dnd'],
+          'utils': ['date-fns', 'uuid']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 })
